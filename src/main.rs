eafbac857da475052684ef7a5085332e661901ee
Paul Colusso <paulcolusso@gmail.com>
2019-07-10 13:44:53 +1000

    x86 only :(

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::thread;
+
 mod settings;
 mod telegram;
 mod datastore;
+mod display;
 
 fn main() {
     let s = settings::load().expect("Invalid settings");
     let d = datastore::load().expect("Datastore failied to init");
 
+    println!("Starting display refresh...");
+    let y = thread::spawn(|| {
+      display::run();
+    });
+
     println!("Starting telegram listener...");
     telegram::listen(s.api_key, d);
+    y.join();
 }
