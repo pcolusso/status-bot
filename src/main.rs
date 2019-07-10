767703120bbf8a7e754256747273fac68024d70d
Paul Colusso <paulcolusso@gmail.com>
2019-07-10 12:49:42 +1000

    added thread safe db access.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod datastore;
 
 fn main() {
     let s = settings::load().expect("Invalid settings");
-    let d = datastore::test().expect("Failed to init datastore");
+    let d = datastore::load().expect("Datastore failied to init");
 
-    println!("api key: {}", s.api_key);
+    println!("Starting telegram listener...");
+    telegram::listen(s.api_key, d);
 }
